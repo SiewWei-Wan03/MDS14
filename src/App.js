35db0ea497f1842452e41fc4e9d67905bfb33c54
SiewWei-Wan03 <swan0236@student.monash.edu>
2024-09-04 13:09:33 +0800

    Added routes (Login and register), change the default route to login route

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import { FaUser, FaEnvelope, FaSignOutAlt } from 'react-icons/fa';
 import './App.css';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 import PatientInfoPage from './pages/PatientInfoPage';
 import RecommendationsPage from './pages/RecommendationsPage';
-import ReassessingPage from './pages/ReassessingPage.js';
-import { database } from './firebase.js';
+import ReassessingPage from './pages/ReassessingPage';
+import SelectPrescriptionPage from './pages/SelectPrescriptionPage';
+import { database } from './firebase';
 import { ref, get } from "firebase/database";
-import SelectPrescriptionPage from './pages/SelectPrescriptionPage.js';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainLayout />} />
+        <Route path="/" element={<LoginPage />} /> {/* Default route should be LoginPage */}
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/patient-info" element={<PatientInfoPage />} />
         <Route path="/select-prescriptions" element={<SelectPrescriptionPage />} />
         <Route path="/recommendations" element={<RecommendationsPage />} />
         <Route path="/reassessing" element={<ReassessingPage />} />
+        <Route path="/main" element={<MainLayout />} /> {/* MainLayout route */}
       </Routes>
     </Router>
   );
@@ -28,8 +33,11 @@ const MainLayout = () => {
   const navigate = useNavigate();
 
   const handleSearchClick = async () => {
-    // Perform any necessary logic before navigating
-    const patientID = document.getElementById('patient-id').value;
+    if (!patientID.trim()) {
+      alert("Please enter a patient ID");
+      return;
+    }
+
     const patientRef = ref(database, `patients/${patientID}`);
 
     try {
@@ -43,7 +51,6 @@ const MainLayout = () => {
     } catch (error) {
       console.error("Error fetching patient data: ", error);
     }
-    // navigate('/patient-info');
   };
 
   const handleKeyPress = (event) => {
@@ -60,11 +67,13 @@ const MainLayout = () => {
           <img src="https://placehold.co/50x50" alt="MDS logo" />
         </div>
         <nav className="nav-icons flex flex-col gap-8 text-green-200">
-          <Link to="/">
+          <Link to="/main">
             <FaUser className="text-2xl" />
           </Link>
           <FaEnvelope className="text-2xl" />
-          <FaSignOutAlt className="text-2xl" />
+          <Link to="/login">
+            <FaSignOutAlt className="text-2xl" />
+          </Link>
         </nav>
       </div>
       {/* Main Content */}
@@ -72,7 +81,6 @@ const MainLayout = () => {
         <div className="text-center">
           <h1 className="heading text-2xl text-green-900 font-semibold mb-4">Please enter the patient ID:</h1>
           <label className="label text-sm text-green-800 mb-2 block" htmlFor="patient-id">Patient ID (e.g. PA12345)</label>
-          {/* <input id="patient-id" type="text" value="PA57481" className="input border border-green-900 rounded px-4 py-2 mb-4" /> */}
           <input 
             id="patient-id" 
             type="text" 
