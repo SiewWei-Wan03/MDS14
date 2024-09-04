2648cc8839e2da8a394e731582a57608d86b2f84
SiewWei-Wan03 <swan0236@student.monash.edu>
2024-09-04 13:10:57 +0800

    Added auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,22 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDCySuYZvAoIO0bB-0uowOq-U8fiT0effQ",
-    authDomain: "emms-5d268.firebaseapp.com",
-    databaseURL: "https://emms-5d268-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "emms-5d268",
-    storageBucket: "emms-5d268.appspot.com",
-    messagingSenderId: "481082533964",
-    appId: "1:481082533964:web:a64b455a07560327556ce9"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
+  apiKey: "AIzaSyDCySuYZvAoIO0bB-0uowOq-U8fiT0effQ",
+  authDomain: "emms-5d268.firebaseapp.com",
+  databaseURL: "https://emms-5d268-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "emms-5d268",
+  storageBucket: "emms-5d268.appspot.com",
+  messagingSenderId: "481082533964",
+  appId: "1:481082533964:web:a64b455a07560327556ce9"
+};
 
-  export { database };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { auth, database };
